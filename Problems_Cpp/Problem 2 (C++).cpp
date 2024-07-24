ca671cdb77ebbc901df5aad4d18730cbf0efd0f2
LvMok <158188942+LvMok@users.noreply.github.com>
2024-07-24 19:32:07 +0900

    Create Problem 2 (C++).cpp

---
+++ Problems_Cpp/Problem 2 (C++).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string str;
+	int a;
+	getline(cin, str);
+	cin >> a;
+
+	for (int i = 0; i < str.size(); i++) {
+		if (i == a - 1) cout << "T";
+		else cout << str[i];
+	}
+}
