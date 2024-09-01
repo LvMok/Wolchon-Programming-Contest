a2f9be8aee053683459fb24e911f34ec3dc9be58
LvMok <158188942+LvMok@users.noreply.github.com>
2024-09-01 14:46:57 +0900

    Create Problem 7 (C++).cpp

---
+++ Problems_Cpp/Problem 7 (C++).cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int w_cnt = 0;
+    bool iscmd = false;
+    vector<string> str;
+
+    while (1) {
+        string inp;
+        getline(cin, inp);
+
+        if (iscmd) {
+            if (inp == ":i") {
+                iscmd = false;
+                continue;
+            }
+            else if (inp == ":dd") {
+                str.clear();
+            }
+            else if (inp == ":X") {
+                str.pop_back();
+            }
+            else if (inp == ":wq") {
+                break;
+            }
+        }
+        else {
+            if (inp == ":e") {
+                iscmd = true;
+                continue;
+            }
+
+
+            if (w_cnt + inp.size() > 30) {
+                cout << "Error" << "\n";
+                continue;
+            }
+            else {
+                w_cnt += inp.size();
+                str.push_back(inp);
+            }
+        }
+    }
+
+    for (int i = 0; i < str.size(); i++) {
+        cout << str[i];
+    }
+}
