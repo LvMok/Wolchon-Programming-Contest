4fb6f3ad1d5101a4b53e907346636ffaa70f41d1
Wonea1 <158188942+LvMok@users.noreply.github.com>
2024-12-16 17:32:03 +0900

    Update Problem 5 (C++).cpp

--- Main/Problems_Cpp/Problem 5 (C++).cpp
+++ Main/Problems_Cpp/Problem 5 (C++).cpp
@@ -18,11 +18,11 @@ int main()
 	int cnt = 0; //뛰어넘은 허들의 갯수 저장
 
 	for (int i = 0; i < num; i++) {
-		int temp = 1;
+		int temp = 0;
 
 		if (hudle[i] > can) { //허들의 높이가 뛰어넘을 수 있는 허들의 최대높이보다 높을 때
 			temp = hudle[i] - can;
-		}
+		}			
 
 		if (temp > hp) {
 			break;
